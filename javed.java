ea31093e95e496edb42ff0bcb8a8e49293b4aa6b
javedkh25 <pjavedkhan91@gmail.com>
2019-07-19 12:33:55 +0530

    all files

---
+++ javed.java
@@ -0,0 +1,10 @@
+If num isn't divisible by any number, flag is false and num is a prime number.
+
+int num = 33, i = 2;
+        boolean flag = false;
+        while(i <= num/2)
+
+Java Programs - Java Programming Examples with Output
+https://beginnersbook.com/2017/09/java-examples/
+To understand a programming language you must practice the programs, this way you can learn the language faster. This page includes java programs on.
+‎Java Program to Calculate · ‎Java Program to Revers
